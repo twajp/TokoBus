866c72070ae2e54c21c9d2de39238f17ee5681c1
TWA <64235761+twajp@users.noreply.github.com>
2023-07-15 16:49:32 +0900

    未対応の特別ダイヤがあるときにダイアログを表示するよう変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/scheduler.dart' show SchedulerBinding;
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:page_view_indicators/circle_page_indicator.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'code.dart';
 import 'CompactTimetableWidget.dart';
 import 'AllTablePage.dart';
@@ -64,6 +66,9 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     mainLoop();
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      showDateDialogIfNeeded(context);
+    });
   }
 
   @override
@@ -219,8 +224,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   Column(
                     children: [
                       Expanded(
-                          flex: 9,
-                          child: AllTableListView(timetable: timetable, deviceHeight: size.height, deviceWidth: size.width / 3.1, showTimetableInfo: false)
+                        flex: 9,
+                        child: AllTableListView(timetable: timetable, deviceHeight: size.height, deviceWidth: size.width / 3.1, showTimetableInfo: false),
                       ),
                       Expanded(
                         flex: 1,
@@ -268,5 +273,52 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+
+  void showDateDialogIfNeeded(BuildContext context) {
+    DateTime currentDate = DateTime.now();
+    List specialDates = [
+      DateTime(2023, 07, 22),
+      DateTime(2023, 08, 05),
+      DateTime(2023, 08, 06),
+      DateTime(2023, 08, 26),
+      DateTime(2023, 09, 02),
+      DateTime(2023, 09, 03),
+      DateTime(2023, 09, 09),
+      DateTime(2023, 09, 23),
+      DateTime(2023, 09, 30),
+    ];
+
+    for (int i = 0; i < specialDates.length; i++) {
+      if (currentDate.year == specialDates[i].year && currentDate.month == specialDates[i].month && currentDate.day == specialDates[i].day) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("本日は特別ダイヤです"),
+              content: const Text("本日の特別ダイヤは未対応です。\n"
+                  "大学ウェブサイトのPDFを確認しますか？"),
+              actions: [
+                TextButton(
+                  child: const Text("閉じる"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: const Text("開く"),
+                  onPressed: () async {
+                    const url = "https://www.waseda.jp/tokorozawa/kg/doc/bus/School_Bus_Timetable_for_Summer_Vacation(from_1_August_2023_to_4_October_%202023).pdf";
+                    await launch(url);
+                    if (!mounted) return;
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
+    }
   }
 }
