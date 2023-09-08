eefd167a57398d75a8bc17630a30178053339930
TWA <64235761+twajp@users.noreply.github.com>
2023-09-08 21:33:41 +0900

    特別ダイヤPDFのURLを追加

--- lib/CreateNextTimetable.dart
+++ lib/CreateNextTimetable.dart
@@ -542,6 +542,8 @@ Map createNextTimetable() {
     },
     "startDate": DateTime(2023, 10, 05),
     "exceptionDates": [
+      DateTime(2023, 10, 07),
+      DateTime(2023, 10, 14),
       DateTime(2023, 10, 21),
       DateTime(2023, 10, 22),
       DateTime(2023, 11, 04),
@@ -549,6 +551,8 @@ Map createNextTimetable() {
     "pdf_url": {
       "default": "https://www.waseda.jp/tokorozawa/kg/doc/bus/School_Bus_Timetable(Tokorozawa_Campus)AY2023_Fall_Semester.pdf",
       "special": {
+        DateTime(2023, 10, 07): "http://www.waseda.jp/tokorozawa/kg/doc/bus/School_Bus_Special_Schedule(Saturday_7th_and_14th_October_2023).pdf",
+        DateTime(2023, 10, 14): "http://www.waseda.jp/tokorozawa/kg/doc/bus/School_Bus_Special_Schedule(Saturday_7th_and_14th_October_2023).pdf",
         DateTime(2023, 10, 21): "https://www.waseda.jp/tokorozawa/kg/doc/bus/School_Bus_Special_Schedule(Saturday%2021st%20and%20Sunday%2022nd%20October2023).pdf",
         DateTime(2023, 10, 22): "https://www.waseda.jp/tokorozawa/kg/doc/bus/School_Bus_Special_Schedule(Saturday%2021st%20and%20Sunday%2022nd%20October2023).pdf",
         DateTime(2023, 11, 04): "http://www.waseda.jp/tokorozawa/kg/doc/bus/School_Bus_Special_Schedule(Saturday_4th_November_2023).pdf",
