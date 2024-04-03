282ae5d4a97af86343ee34766a65e28d79acaa37
twajp <twajp@users.noreply.github.com>
2024-04-03 14:18:12 +0000

    deploy: b83b7a2001ee4b9816d2cbbbe5b1b2beba161c32

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "2aa3f4738a746f514d11f9a5697db696",
-"/": "2aa3f4738a746f514d11f9a5697db696",
+const RESOURCES = {"index.html": "fbbbb59922d5d5ae8191abdf0a21b3fd",
+"/": "fbbbb59922d5d5ae8191abdf0a21b3fd",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
