e6e5ac5dc9287cbb5ef34d5824d042ac471a140e
TWA <64235761+twajp@users.noreply.github.com>
2025-02-04 17:33:31 +0900

    2025年度の授業を行う祝日と臨時の休業日に対応

--- lib/data/holiday_config.dart
+++ lib/data/holiday_config.dart
@@ -1,26 +1,21 @@
+// 大学暦ページ(下記URL)からの情報を記載するファイル
+// https://www.waseda.jp/top/about/work/organizations/academic-affairs-division/academic-calendar
+
 // 授業を行う祝日
 List<DateTime> exceptionalHolidays = [
-  DateTime(2024, 04, 29),
-  DateTime(2024, 07, 15),
-  DateTime(2024, 10, 14),
-  DateTime(2024, 11, 23),
+  DateTime(2025, 04, 29),
+  DateTime(2025, 07, 21),
+  DateTime(2025, 10, 13),
+  DateTime(2025, 11, 03),
+  DateTime(2025, 11, 24),
 ];
 
-// 臨時の休業日, 追加で祝日扱いする日
+// 臨時の休業日
 List<DateTime> additionalHolidays = [
-  DateTime(2024, 08, 09),
-  DateTime(2024, 08, 12),
-  DateTime(2024, 08, 13),
-  DateTime(2024, 08, 14),
-  DateTime(2024, 08, 15),
-  DateTime(2024, 08, 16),
-  DateTime(2024, 08, 19),
-  DateTime(2024, 09, 16),
-  DateTime(2024, 09, 23),
-  DateTime(2024, 11, 01),
-  DateTime(2024, 12, 26),
-  DateTime(2024, 12, 27),
-  DateTime(2025, 02, 11),
-  DateTime(2025, 02, 24),
-  DateTime(2025, 03, 20),
+  DateTime(2025, 08, 07),
+  DateTime(2025, 08, 08),
+  DateTime(2025, 10, 30),
+  DateTime(2025, 10, 31),
+  DateTime(2025, 12, 26),
+  DateTime(2026, 01, 06),
 ];
