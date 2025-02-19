d7b0211f4cd4666aae91e8e937c6fb6c0ce4fac2
TWA <64235761+twajp@users.noreply.github.com>
2025-02-19 14:41:37 +0900

    feat: このメッセージを再表示しないオプションを追加

--- lib/services/json_alart_handler.dart
+++ lib/services/json_alart_handler.dart
@@ -2,61 +2,103 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> jsonAlertHandler({required BuildContext context}) async {
   final url = Uri.parse('https://raw.githubusercontent.com/twajp/TokoBus/master/data/dialog.json');
-  // final url = Uri.parse('https://gist.githubusercontent.com/twajp/5effe414d9a1f1f711c5a1075d195646/raw/a8166965bf1266433e42b12f42b13adf6c01e138/dialog.json');
+  // final url = Uri.parse('../../data/dialog.json'); // テスト用
   final response = await http.get(url);
 
   if (response.statusCode == 200) {
     final jsonData = json.decode(response.body);
-    if (jsonData['flag'] == true) {
+    // final jsonData = jsonDecode(utf8.decode(response.bodyBytes)); // テスト用
+    final prefs = await SharedPreferences.getInstance();
+    final int? ignoredId = prefs.getInt('ignored_alert_id');
+
+    if (jsonData['flag'] && jsonData['id'] != ignoredId) {
       // State が存在する（つまりウィジェットがまだツリーにある）ことを確認してから実行
       if (!context.mounted) return;
-      _showJsonAlert(context: context, jsonData: jsonData);
+      _showJsonAlert(context: context, jsonData: jsonData, prefs: prefs);
     }
   } else {
     throw Exception('Failed to load JSON data');
   }
 }
 
-void _showJsonAlert({required BuildContext context, required Map<String, dynamic> jsonData}) {
+void _showJsonAlert({required BuildContext context, required Map<String, dynamic> jsonData, required SharedPreferences prefs}) {
+  bool doNotShowAgain = jsonData['defaultCheckbox'] ?? false;
+  bool allowDoNotShowAgain = jsonData['allowDoNotShowAgain'] ?? false;
+
   showDialog(
     context: context,
     builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text(jsonData['title']),
-        content: Text(jsonData['content']),
-        actions: [
-          TextButton(
-            child: Text(
-              '閉じる',
-              style: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
+      return StatefulBuilder(
+        builder: (context, setState) {
+          return AlertDialog(
+            title: Text(jsonData['title']),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              spacing: 10,
+              children: [
+                Text(jsonData['content']),
+                if (allowDoNotShowAgain)
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        doNotShowAgain = !doNotShowAgain;
+                      });
+                    },
+                    child: Row(
+                      children: [
+                        Checkbox(
+                          value: doNotShowAgain,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              doNotShowAgain = value ?? false;
+                            });
+                          },
+                        ),
+                        Text('このメッセージを再表示しない'),
+                      ],
+                    ),
+                  ),
+              ],
             ),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          if (jsonData['url'] != '') ...{
-            TextButton(
-              onPressed: () async {
-                final navigator = Navigator.of(context);
-                final url = jsonData['url'];
-                final uri = Uri.parse(url);
-                if (!await launchUrl(uri)) {
-                  throw Exception('Could not launch $url');
-                }
-                if (navigator.canPop()) {
-                  navigator.pop();
-                }
-              },
-              child: Text(
-                '開く',
-                style: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
+            actions: [
+              TextButton(
+                child: Text(
+                  '閉じる',
+                  style: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
+                ),
+                onPressed: () {
+                  if (allowDoNotShowAgain && doNotShowAgain) {
+                    prefs.setInt('ignored_alert_id', jsonData['id']);
+                  }
+                  Navigator.of(context).pop();
+                },
               ),
-            ),
-          },
-        ],
+              if (jsonData['url'] != '') ...{
+                TextButton(
+                  onPressed: () async {
+                    final navigator = Navigator.of(context);
+                    final url = jsonData['url'];
+                    final uri = Uri.parse(url);
+                    if (!await launchUrl(uri)) {
+                      throw Exception('Could not launch $url');
+                    }
+                    if (navigator.canPop()) {
+                      navigator.pop();
+                    }
+                  },
+                  child: Text(
+                    '開く',
+                    style: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
+                  ),
+                ),
+              },
+            ],
+          );
+        },
       );
     },
   );
